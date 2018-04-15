b27b4e1ca170ba1b3ab51c8abebe36441de78477
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-16 09:03:42 +1200

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send(":arrow_down: __**Permanent Invite Link**__ :arrow_down:\n \nhttps://discord.gg/ATcsYHA")
+}
+
+module.exports.help = {
+    name: "server"
+}
