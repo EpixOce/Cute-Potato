2c963af67187b614894b0a943dfd89563c3e9c9f
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:04:42 +1200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
@@ -29,3 +29,4 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
     name: "poll"
 }
+*/
