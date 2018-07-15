e604a4a35bf59fb08a2015787ec79bf8e7b48b88
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:05:02 +1200

    Delete chop.js

--- commands/chop.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send(":evergreen_tree: " + `${message.author.username}` + " went to chop some wood but got scared by a Bear and ran away.")
-}
-
-module.exports.help = {
-    name: "chop"
-}
