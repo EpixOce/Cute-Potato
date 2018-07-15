61dfe7c7fc90edadbc9c778729531dfa50010e6b
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:03:08 +1200

    Delete forage.js

--- commands/forage.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send(":blossom: " + `${message.author.username}` + " went foraging and got stung by Bees.")
-}
-
-module.exports.help = {
-    name: "forage"
-}
