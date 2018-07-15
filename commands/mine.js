da9d1002dad79cbdd7ea73f2a0bb805001b81994
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:03:02 +1200

    Delete mine.js

--- commands/mine.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send(":pick: " + `${message.author.username}` + " found a Cinnabar, got poisoned and died.")
-}
-
-module.exports.help = {
-    name: "mine"
-}
