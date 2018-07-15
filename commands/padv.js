bddd5adfb71c1b4c6cb76ea855b016498269ec52
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:02:33 +1200

    Delete padv.js

--- commands/padv.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.channel.send(":x: **The DiscordRPG prefix in this server is `,`**")
-}
-
-module.exports.help = {
-    name: "padv"
-}
