e4822fff2861e28ee639b2b711aab51396d30a4b
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-07-16 11:04:23 +1200

    Update spin.js

--- commands/spin.js
+++ commands/spin.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let replies = [
@@ -23,3 +23,4 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
     name: "spin"
 }
+*/
